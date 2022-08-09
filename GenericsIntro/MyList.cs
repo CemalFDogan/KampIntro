3e712f351343946972b97d9b329e9e6bf921e026
CemalFDogan <cxexmxaxl4409@gmail.com>
2022-08-09 11:25:01 +0300

    Update

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>               // Bir array referans tip olduğu için new'lenme zorunluluğu vardır. 0 olarak
+    {                            // new'lemek istiyoruz. Bu yüzden bir class new'lendiğinde çalışan bloğa "constructor" denir.     
+        T[] items;
+        
+        // constructor
+        public MyList()
+        {
+            items = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+        }
+    }
+}
---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------------Array------------");
+
+            string[] isimler = new string[] { "Engin", "Kerem", "Murat", "Halil" };
+
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            Console.WriteLine(isimler[3]);
+
+            isimler = new string[5];        // Burada aynı ada sahip başka bir array oluşturduğumuz için 
+            isimler[4] = "İlker";           // sonradan atadığımız 4. indeksteki İlker'i yazdı ama
+            Console.WriteLine(isimler[4]);  // en başında tanımlamış olduğumuz şeylere ulaşamıyoruz.
+            Console.WriteLine(isimler[0]);
+
+            Console.WriteLine("----------------------List-------------");
+
+            //List<string> isimler2 = new List<string>();     // 1. YOL
+            //isimler2.Add("Engin");
+
+            List<string> isimler2 = new List<string> { "Engin", "Kerem", "Murat", "Halil" };
+
+            Console.WriteLine("\n" + "Yeni eleman eklenmeden önce");
+
+            foreach (string isim in isimler2)
+            {
+                Console.WriteLine(isim);
+            }
+
+            Console.WriteLine("\n" + "Yeni eleman eklendikten sonra");
+
+            isimler2.Add("Cemal");
+            foreach (string isim in isimler2)
+            {
+                Console.WriteLine(isim);
+            }
+        }
+    }
+}
